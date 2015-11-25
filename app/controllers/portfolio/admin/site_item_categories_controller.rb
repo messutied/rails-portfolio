f1367d0d109a89a6dd5ec0b3bb5c885bdbb5564d
Eduardo Messuti <messuti.edd@gmail.com>
2015-11-25 09:57:52 -0400

    small refactor to categories

--- app/controllers/portfolio/admin/site_item_categories_controller.rb
+++ app/controllers/portfolio/admin/site_item_categories_controller.rb
@@ -2,48 +2,52 @@ require_dependency "portfolio/application_controller"
 
 module Portfolio
   class Admin::SiteItemCategoriesController < ApplicationController
-    before_action :set_admin_site_item_category, only: [:show, :edit, :update, :destroy]
     before_action Portfolio.auth_action
+    before_action :set_admin_site_item_category,
+                  only: [:show, :edit, :update, :destroy]
     
     layout 'portfolio/admin'
 
     def index
-      @admin_site_item_categories = SiteItemCategory.all
+      @categories = SiteItemCategory.all
     end
 
     def new
-      @admin_site_item_category = SiteItemCategory.new
+      @categ = SiteItemCategory.new
     end
 
     def edit
     end
 
     def create
-      @admin_site_item_category = SiteItemCategory.new(admin_site_item_category_params)
+      @categ = SiteItemCategory.new(admin_site_item_category_params)
 
-      if @admin_site_item_category.save
-        redirect_to admin_site_item_categories_path, notice: 'Site item category was successfully created.'
+      if @categ.save
+        redirect_to admin_site_item_categories_path,
+          notice: 'Site item category was successfully created.'
       else
         render :new
       end
     end
 
     def update
-      if @admin_site_item_category.update(admin_site_item_category_params)
-        redirect_to admin_site_item_categories_path, notice: 'Site item category was successfully updated.'
+      if @categ.update(admin_site_item_category_params)
+        redirect_to admin_site_item_categories_path,
+          notice: 'Site item category was successfully updated.'
       else
         render :edit
       end
     end
 
     def destroy
-      @admin_site_item_category.destroy
-      redirect_to admin_site_item_categories_path, notice: 'Site item category was successfully destroyed.'
+      @categ.destroy
+      redirect_to admin_site_item_categories_path,
+        notice: 'Site item category was successfully destroyed.'
     end
 
     private
       def set_admin_site_item_category
-        @admin_site_item_category = SiteItemCategory.find(params[:id])
+        @categ = SiteItemCategory.find(params[:id])
       end
 
       def admin_site_item_category_params
