457372004de2057aefdcd85f55c87abd6970addd
Eduardo Messuti <messuti.edd@gmail.com>
2016-01-26 17:11:39 -0400

    small refactoring to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Portfolio::Engine.routes.draw do
   if Portfolio.auth_method == :portfolio_admin_user
     devise_for :admin_users, class_name: 'Portfolio::AdminUser', module: :devise
   end
-  
+
   namespace :admin do
     post 'markdown_preview' => 'api#markdown_preview'
-    
+
     resources :site_item_categories
     resources :site_item_tags
     resources :social_links
@@ -26,11 +26,20 @@ Portfolio::Engine.routes.draw do
 
   Portfolio::SiteItem.subclasses.each do |klass|
     resource_name = klass.human_name
-    res = resource_name.pluralize
-    dname = (klass.display_name || res).downcase
-    get "/:portfolio_key/#{dname}/:id-:title" => "site_#{res}#show", as: "show_portfolio_#{resource_name}"
-    get "/:portfolio_key/#{dname}" => "site_#{res}#index", as: "portfolio_#{res}"
-    get "/:portfolio_key/#{dname}/tagged/:tag_key" => "site_#{res}#index", as: "portfolio_#{res}_tagged"
+    resource_plural_name = resource_name.pluralize
+    resource_display_name = (klass.display_name || resource_plural_name).downcase
+
+    get "/:portfolio_key/#{resource_display_name}/:id-:title" =>
+        "site_#{resource_plural_name}#show",
+        as: "show_portfolio_#{resource_name}"
+
+    get "/:portfolio_key/#{resource_display_name}" =>
+        "site_#{resource_plural_name}#index",
+        as: "portfolio_#{resource_plural_name}"
+
+    get "/:portfolio_key/#{resource_display_name}/tagged/:tag_key" =>
+        "site_#{resource_plural_name}#index",
+        as: "portfolio_#{resource_plural_name}_tagged"
   end
 
   get '/:portfolio_key' => 'sites#show_by_key', as: :show_portfolio
